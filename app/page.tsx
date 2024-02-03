0dbf19f21a08f8b875de1b9bed7a4b8989115d8b
RajendranN2000 <rajurajendran.nachiappan@gmail.com>
2024-02-03 18:57:54 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div className="">My first next project to deploy</div>
+   <div className="">Thambi Tamizh</div>
   );
 }
